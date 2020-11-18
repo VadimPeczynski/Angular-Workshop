c453ff996d6034f4342258e0f3cf383791c47bd5
Vadim Peczynski <Vadim.Peczynski@external.sennheiser.com>
2020-11-18 11:33:33 +0100

    Initial state of final project for Workshop 3

---
+++ 3/final/src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { HeroListComponent } from './hero-list/hero-list.component';
+import { FormsModule } from '@angular/forms';
+import { SecretPipe } from './hero-list/secret.pipe';
+import { FistComponent } from './shared/fist/fist.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { HomeComponent } from './home/home.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FistComponent,
+    HeroListComponent,
+    HomeComponent,
+    NavBarComponent,
+    SecretPipe,
+  ],
+  imports: [BrowserModule, FormsModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
