f1d8afb8b99c4038871691bab022c06b70952b48
Vadim Peczynski <Vadim.Peczynski@external.sennheiser.com>
2020-11-19 15:12:07 +0100

    HeroDetail component

--- 3/final/src/app/hero-detail/hero-detail.component.ts
+++ 3/final/src/app/hero-detail/hero-detail.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-hero-detail',
   templateUrl: './hero-detail.component.html',
-  styleUrls: ['./hero-detail.component.scss']
+  styleUrls: ['./hero-detail.component.scss'],
 })
 export class HeroDetailComponent implements OnInit {
-
-  constructor() { }
+  pageTitle = 'Informacje o herosie';
+  constructor(private activatedRoute: ActivatedRoute, private router: Router) {}
 
   ngOnInit(): void {
+    const id = +this.activatedRoute.snapshot.paramMap.get('id');
+    this.pageTitle += `: ${id}`;
   }
 
+  onClick(): void {
+    this.router.navigate(['/heroes']);
+  }
 }
